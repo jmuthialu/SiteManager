8e091357764f0fac4c8da57c93cf7f5e082b3c0f
jmuthialu <jmuthialu@yahoo.com>
2023-08-31 19:09:00 -0400

    code cleanup

--- app/src/main/java/com/jay/sitemanager/ble/BLEFacade.kt
+++ app/src/main/java/com/jay/sitemanager/ble/BLEFacade.kt
@@ -1,5 +1,6 @@
 package com.jay.sitemanager.ble
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
@@ -9,7 +10,9 @@ import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.jay.sitemanager.dataModels.BLEDevice
 import java.util.HashMap
 
@@ -18,9 +21,6 @@ class BLEFacade(context: Context) {
     companion object {
         const val TAG = "$$$"
     }
-
-    private val scanResultDictionary: MutableMap<String, ScanResult> = HashMap()
-    private val bleDevices = mutableListOf<BLEDevice>()
 
     private val bluetoothManager =
         context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
@@ -31,6 +31,8 @@ class BLEFacade(context: Context) {
     private var bleScanner: BluetoothLeScanner? = null
     private var scanFilters: List<ScanFilter> = listOf()
     private var scanSettings: ScanSettings = ScanSettings.Builder().build()
+    private val scanResultDictionary: MutableMap<String, ScanResult> = HashMap()
+    private val bleDevices = mutableListOf<BLEDevice>()
 
     init {
         if (bluetoothAdapter?.bluetoothLeScanner != null) {
@@ -38,8 +40,6 @@ class BLEFacade(context: Context) {
             bleScanner = bluetoothAdapter?.bluetoothLeScanner
             scanSettings = bleScanSettings()
             Log.d(TAG, "isBluetoothRadioEnabled: ${isBluetoothRadioEnabled()}")
-//            startScan()
-
         } else {
             Log.d(TAG, "BLEFacade: bluetoothLeScanner is null")
         }
@@ -51,8 +51,6 @@ class BLEFacade(context: Context) {
         return scanSettingsBuilder.build()
     }
 
-    // Checks if BLE radio at phone level is turned on
-    // bluetoothLeScanner will be null if the radio is turned OFF
     fun isBluetoothRadioEnabled(): Boolean {
         return bluetoothAdapter?.isEnabled ?: false
     }
@@ -63,9 +61,10 @@ class BLEFacade(context: Context) {
         bleScanner?.startScan(scanFilters, scanSettings, bleScanCallback)
     }
 
-//    fun stopScan() {
-//        bleScanner?.stopScan(bleScanCallback)
-//    }
+    @SuppressLint("MissingPermission")
+    fun stopScan() {
+        bleScanner?.stopScan(bleScanCallback)
+    }
 
     private val bleScanCallback: ScanCallback = object : ScanCallback() {
 
@@ -73,12 +72,15 @@ class BLEFacade(context: Context) {
             val address = result.device.address
             val serviceUUID = result.scanRecord?.serviceUuids?.first().toString()
             val rssi = result.rssi
+
+            // Add only unique devices
             if  (!scanResultDictionary.containsKey(address)) {
                 scanResultDictionary[address] = result
                 val bleDevice = BLEDevice(address, serviceUUID, rssi)
                 bleDevices.add(bleDevice)
                 Log.d(TAG, "$$$ BLE Device added: $bleDevice - Total count: ${bleDevices.size}")
             } else {
+                // Update RSSI if device already exists
                 bleDevices.filter { it.address == address }.forEach {
                     it.rssi = rssi
                     Log.d(TAG, "$$$ BLE Device updated: $it")
@@ -86,15 +88,10 @@ class BLEFacade(context: Context) {
             }
         }
 
-        override fun onBatchScanResults(results: List<ScanResult>) {
-            Log.d(TAG, "onBatchScanResults called, with " + results.size)
-        }
-
         override fun onScanFailed(errorCode: Int) {
             Log.d(TAG, "onScanFailed with error code: $errorCode")
             super.onScanFailed(errorCode)
         }
     }
-
 
 }
