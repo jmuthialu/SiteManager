99be5c68fad73c28e37c8a302ecf751f69d39aba
jmuthialu <jmuthialu@yahoo.com>
2023-09-01 12:18:35 -0400

    Extending user props

--- app/src/main/java/com/jay/sitemanager/presentation/users/remoteUsers/RemoteUserDetailView.kt
+++ app/src/main/java/com/jay/sitemanager/presentation/users/remoteUsers/RemoteUserDetailView.kt
@@ -1,12 +1,45 @@
 package com.jay.sitemanager.presentation.users.remoteUsers
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jay.sitemanager.dataModels.RemoteUser
 
 @Composable
 fun RemoteUserDetailView(user: RemoteUser?) {
 
-    Text(text = user?.name ?: "No Name")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        user?.name?.let {
+            Text(text = "Name: $it")
+        }
+
+        user?.address?.street.let {
+            Text(text = "Street: $it")
+        }
+
+        user?.address?.geo?.lat.let {
+            Text(text = "Lat: $it")
+        }
+
+        user?.address?.geo?.lng.let {
+            Text(text = "Lon: $it")
+        }
+
+        user?.company?.name?.let {
+            Text(text = "Company Name: $it")
+        }
+
+
+    }
+
+
 
 }
